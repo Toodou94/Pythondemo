9f2f016f9e01060b2f0ef2c6cb656b715e07747e
Lynn Zhang <zhangli940427@gmail.com>
2017-06-28 16:46:36 +0800

    change 1

---
+++ tftest.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 28 11:31:29 2017
+
+@author: zhangli
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+ 
+gold,chihh = 400,400
+
+gold_height = 40 + 10* np.random.randn(gold)
+chihh_height = 25 + 6* np.random.randn(chihh)
+
+plt.hist([gold_height, chihh_height],stacked = True,color=['r','b'])
+plt.show()
